c24222035deee6c7b10eaf4122f9f3d6716af1d1
Mark Jimenez <mark.jimenez@ucalgary.ca>
2024-03-11 23:46:30 -0600

    Change elements to DOM

--- login_script.js
+++ login_script.js
@@ -1,38 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const main = document.querySelector('main');
+    const loginDiv = document.createElement('div');
+    loginDiv.id = 'loginDiv';
+    const loginMsg = document.createElement('p');
+    loginMsg.id = 'login-msg';
+    loginDiv.appendChild(loginMsg);
+    main.appendChild(loginDiv);
 
-
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const messages = [];
-    const login = document.getElementById('loginDiv');
-    const loginMsg = document.getElementById('login-msg');
-    fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('API call unsuccessful');
-            
-            }
-            return response.json();
-        })
-        .then(data => {
-            const username = document.getElementById('username').value;
-            const password = document.getElementById('password').value;
-            let verified = 0;
-            
-            data.forEach(user =>{
-                if (user.name === username && user.email === password) {
-                    messages.push('Login successful');
-                    verified = 1;
-                
+    document.querySelector('form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        const messages = [];
+        const loginMsg = document.getElementById('login-msg');
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('API call unsuccessful');
                 }
+                return response.json();
+            })
+            .then(data => {
+                const username = document.getElementById('username').value;
+                const password = document.getElementById('password').value;
+                let verified = 0;
+
+                data.forEach(user => {
+                    if (user.name === username && user.email === password) {
+                        messages.push('Login successful');
+                        verified = 1;
+                    }
+                });
+                if (verified === 0) {
+                    messages.push('Username or password is incorrect. Please try again.');
+                }
+
+                loginDiv.style.backgroundColor = "#f9f9f9";
+                loginDiv.style.border = "2px solid black";
+                loginDiv.style.padding = "10px";
+                loginDiv.style.margin = "20px";
+                loginMsg.innerHTML = messages.join('<br>');
             });
-            if (verified === 0) {
-                messages.push('Username or password is incorrect. Please try again.');
-            }
-
-            login.style.border = "2px solid #f9f9f9";
-            login.style.padding = "10px";
-            login.style.margin = "20px";
-            loginMsg.innerHTML = messages.join('<br>');
+    });
 });
-});
-
--- signup_script.js
+++ signup_script.js
@@ -1,88 +1,91 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const main = document.querySelector('main');
+    const errorDiv = document.createElement('div');
+    errorDiv.id = 'error';
+    const errorMsg = document.createElement('p');
+    errorMsg.id = 'error-msg';
+    errorDiv.appendChild(errorMsg);
+    main.appendChild(errorDiv);
 
-//Signup Page
-//Signup Page
-//Signup Page
+    const email = document.getElementById('email');
+    const confirmPassword = document.getElementById('confirm-password');
+    const password = document.getElementById('password');
+    const username = document.getElementById('username');
+    const errorElement = document.getElementById('error-msg');
 
-const email = document.getElementById('email');
-const confirmpassword = document.getElementById('confirm-password');
-const Password = document.getElementById('password');
-const Username = document.getElementById('username');
-const errorElement = document.getElementById('error-msg');
+    document.getElementById('signup-form').addEventListener('submit', function(error) {
+        const messages = [];
 
-document.getElementById('signup-form').addEventListener('submit', (error) => {
+        if (username.value === '' || username.value == null) {
+            messages.push('Username is required');
+        }
+        if (!/^[a-zA-Z]/.test(username.value)) {
+            messages.push('Username must start with a letter');
+        }
+        if (username.value.length < 3 || username.value.length > 20) {
+            messages.push('Username length must be between 3 and 20 characters long');
+        }
 
-    const messages = [];
+        if (!/^[\w-]+$/.test(username.value)) {
+            messages.push('Username can only contain letters, numbers, hyphens, and underscores');
+        }
 
-    if (Username.value === '' || Username.value == null) {
-        messages.push('Username is required');
-    }
-    if (!/^[a-zA-Z]/.test(Username.value)) {
-        messages.push('Username must start with a letter');
-    }
-    if (Username.value.length < 3 || Username.value.length > 20) {
-        messages.push('Username length must be between 3 and 20 characters long');
-    }
+        if (password.value === '' || password.value == null) {
+            messages.push('Password is required');
+        }
 
+        if (password.value.length < 8) {
+            messages.push('Password length must be at least 8 characters long');
+        }
 
-    if (!/^[\w-]+$/.test(Username.value)) {
-        messages.push('Username can only contain letters, numbers, hyphens, and underscores');
-    }
+        if (!/[a-z]/.test(password.value) || !/[A-Z]/.test(password.value)) {
+            messages.push('Password must contain at least one upper case and one lower case letter');
+        }
+        if (!/\d/.test(password.value)) {
+            messages.push('Password must contain at least one number');
+        }
 
-    if (Password.value === '' || Password.value == null) {
-        messages.push('Password is required');
-    }
+        if (!/[!@#$%^&*()-_=+\[\]{}|;:'",.<>?/`~]/.test(password.value)) {
+            messages.push('Password must contain at least one special character');
+        }
+        if (password.value.includes(' ')) {
+            messages.push('Password cannot contain spaces');
+        }
 
-    if (Password.value.length < 8) {
-        messages.push('Password length must be at least 8 characters long');
-    }
-    
-    if (!/[a-z]/.test(Password.value) || !/[A-Z]/.test(Password.value)) {
-        messages.push('Password must contain at least one upper case and one lower case letter');
-    }
-    if (!/\d/.test(Password.value)) {
-        messages.push('Password must contain at least one number');
-    }
+        if (confirmPassword.value === '' || confirmPassword.value == null) {
+            messages.push('Confirm Password is required');
+        }
 
-    if (!/[!@#$%^&*()-_=+\[\]{}|;:'",.<>?/`~]/.test(Password.value)) {
-        messages.push('Password must contain at least one special character');
-    }
-    if (Password.value.includes(' ')) {
-        messages.push('Password cannot contain spaces');
-    }
+        if (confirmPassword.value !== password.value) {
+            messages.push('Passwords do not match');
+        }
 
-    if (confirmpassword.value === '' || confirmpassword.value == null) {
-        messages.push('Confirm Password is required');
-    }
+        if (email.value === '' || email.value == null) {
+            messages.push('Email is required');
+        }
 
-    if (confirmpassword.value !== Password.value) {
-        messages.push('Passwords do not match');
-    }
+        if (!/\S+@\S+\.\S+/.test(email.value)) {
+            messages.push('Email must be a valid email address format');
+        }
+        if (email.value.includes(' ')) {
+            messages.push('Email cannot contain spaces');
+        }
 
-    if (email.value === '' || email.value == null) {
-        messages.push('Email is required');
-    }
+        if (!email.value.includes('@') || !email.value.includes('.')) {
+            messages.push('Email must contain an "@" symbol followed by a domain name i.e(.com, .net, .io)');
+        }
 
-    if (!/\S+@\S+\.\S+/.test(email.value)) {
-        messages.push('Email must be a valid email address format');
-    }
-    if (email.value.includes(' ')) {
-        messages.push('Email cannot contain spaces');
-    }
-
-    if (!email.value.includes('@') || !email.value.includes('.')) {
-        messages.push('Email must contain an "@" symbol followed by a domain name i.e(.com, .net, .io)');
-    }
-    const errorDiv = document.getElementById('error');
-    errorDiv.style.border = "2px solid #f9f9f9";
-    errorDiv.style.padding = "10px";
-    errorDiv.style.margin = "20px";
-    
-
-
-    if (messages.length > 0) {
-        error.preventDefault();
-        errorElement.innerText = messages.join('\n');
+        const errorDiv = document.getElementById('error');
+        errorDiv.style.border = "2px";
+        errorDiv.style.backgroundColor = "#f9f9f9";
+        errorDiv.style.padding = "10px";
+        errorDiv.style.margin = "20px";
         
-    }
-});
+    
 
+        if (messages.length > 0) {
+            error.preventDefault();
+            errorElement.innerText = messages.join('\n');
+        }
+    });
+});
