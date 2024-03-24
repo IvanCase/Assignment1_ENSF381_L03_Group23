5cdc2200c820c7e414b23de754aeacbbb3e4bbe9
Mark Jimenez <mark.jimenez@ucalgary.ca>
2024-03-23 20:32:43 -0600

    updated the header

---
+++ Header.js
@@ -0,0 +1,13 @@
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+//Name : Header.js
+//Assignment : Assignment 4
+//Author ( s ) : Mark Jimenez, Ron Ivan Ibe
+//Submission : Mar 25, 2024
+//Description : Header component of the website
+//= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+
+
+
+//ADD THE HEADER COMPONENT HERE
