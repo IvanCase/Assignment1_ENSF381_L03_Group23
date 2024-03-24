d517bec53ddda6cea21d7b20aa80461fcdee5a12
Mark Jimenez <mark.jimenez@ucalgary.ca>
2024-03-23 21:51:37 -0600

    Update arranging

--- my-ecommerce-app/src/components/ProductPage.js
+++ my-ecommerce-app/src/components/ProductPage.js
@@ -4,17 +4,22 @@ import products from '../data/product.js';
 
 const ProductPage = () => {
     return (
-        <div>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
             {products.map((product) => (
-                <div key={product.id}>
+                <div key={product.id} style={{ marginBottom: '20px' }}>
                     <h2>{product.name}</h2>
-                    <img src={product.image} alt={product.name} />
+                    <img src={product.image} alt={product.name} style={{ width: '150px', height: '150px' }} />
                     <p>{product.description}</p>
                     <p>Price: ${product.price}</p>
+
+                  
+                    <button>Add to Cart</button>
                 </div>
+            
             ))}
         </div>
-    );
-};
 
+
+    );
+}
 export default ProductPage;
