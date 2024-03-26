20062f659fc4d13b78ce2ea0b8391b2fb8a61779
Ivan Ibe <ivan.ibe@ucalgary.ca>
2024-03-25 21:15:35 -0600

    commit

--- my-ecommerce-app/src/App.js
+++ my-ecommerce-app/src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import Productpage from './components/Productpage.js'; 
-import './App.css';
-import { CartContext, CartContextProvider } from './components/cart.js';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Homepage from './components/Homepage';
+import ProductPage from './components/ProductPage';
 
 function App() {
   return (
-    <div className="App">
-      <CartContextProvider>
-          <Productpage />
-
-          
-      </CartContextProvider>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/Homepage" element={<Homepage />} />
+        <Route path="/products" element={<ProductPage />} />
+      </Routes>
+    </Router>
   );
 }
-
 
 export default App;
--- my-ecommerce-app/src/components/ProductPage.js
+++ my-ecommerce-app/src/components/ProductPage.js
@@ -2,50 +2,15 @@ import React, { useState } from 'react';
 import { useContext } from 'react';
 import products from '../data/product.js';
 import { CartContext } from "../components/cart.js";
+import Header from './Header';
+import Footer from './Footer';
 
-const Productpage = (props) => {
-    const { AddToCart, cartItems, RemoveFromCart } = useContext(CartContext);
-    const [hoveredProductId, setHoveredProductId] = useState(null);
+const Productpage = () => {
 
     return (
-        <div style={{ display: 'flex', flexDirection: 'row' }}>
-            <div className="ProductItem" style={{ alignItems: 'flex-start' }}>
-                {products.map((product) => (
-                    <div key={product.id}>
-                        <img src={product.image} alt={product.name} style={{ width: '150px', height: '150px' }} />
-                        <p
-                            onMouseEnter={() => setHoveredProductId(product.id)}
-                            onMouseLeave={() => setHoveredProductId(null)}
-                        >
-                            {product.name}
-                        </p>
-                        {hoveredProductId === product.id && <p>{product.description}</p>}
-                        <p>Price: ${product.price}</p>
-                        <button className="addToCartBtn" onClick={() => AddToCart(product.id)}>Add to Cart</button>
-                    </div>
-                ))}
-            </div>
-            <div style={{ flex: 1 }}>
-                <h1><b>Shopping Cart</b></h1>
-                <div>
-                    {products.map((product) => {
-                        if (cartItems[product.id] !== 0) {
-                            return (
-                                <div key={product.id}>
-                                    <img src={product.image} alt={product.name} style={{ width: '50px', height: '50px' }} />
-                                    <div>
-                                        <h3>{product.name}</h3>
-                                        <p>Price: ${product.price}</p>
-                                        <p>Quantity: {cartItems[product.id]}</p>
-                                        <button className="addToCartBtn" onClick={() => RemoveFromCart(product.id)}>Remove</button>
-                                    </div>
-                                </div>
-                            );
-                        }
-                    })}
-                </div>
-                <p>Total (in cart): ${products.reduce((acc, product) => acc + cartItems[product.id] * product.price, 0).toFixed(2)}</p>
-            </div>
+        <div>
+            <Header />
+            <Footer />
         </div>
     );
 };
